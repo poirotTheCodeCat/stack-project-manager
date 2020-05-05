4cf1b9566d782b41895b0d302af7eb5b2352d3ea
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-05-05 16:12:10 -0400

    Added styling to app.css

---
+++ front-end/src/components/homeComponents/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function Home() {
+  return <div>Hello World!</div>;
+}
+
+export default Home;
