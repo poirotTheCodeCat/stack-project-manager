8472f12cd31e2f9705cbf13b3b7f46e847b9b7ad
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-05-05 16:20:42 -0400

    created Contact page component

---
+++ front-end/src/components/homeComponents/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <h2>Contact me at</h2>
+    </div>
+  );
+}
+
+export default Contact;
