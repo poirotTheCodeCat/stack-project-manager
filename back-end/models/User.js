68203ea342ae8be9bc07f9761ccd64d2e1e347a6
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-05-05 20:08:44 -0400

    Created User Schema

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,2 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+// Create the User Schema
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passHash: {
+    type: String,
+    required: true,
+  },
+  registerDate: {
+    type: String,
+    default: Date.now(),
+  },
+});
+
+module.exports = User = mongoose.model("User", userSchema); // export the user model
