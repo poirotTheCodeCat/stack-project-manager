2d9db2857f30b3560813634ebadef3e9e2abe225
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-05-06 12:38:48 -0400

    Added database access to the users api

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
-// route - GET api/users
+// route - GET /api/users
 // description - Retrieves user information
 // access TBD
 router.get("/", (req, res) => {
@@ -9,6 +10,50 @@ router.get("/", (req, res) => {
     firstName: "Sherlock",
     lastName: "Holmes",
     email: "pipedetective@mystery.com",
+  });
+});
+
+// route - POST /api/users
+// description - Store a new user in the database
+// access - Public
+router.post("/", (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  // Simple validation
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).json({ msg: "Credentials are missing" });
+  }
+
+  // Check for existing user
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    // create new user
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      passHash,
+    });
+
+    // Salt and hash the password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.passHash, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.passHash = hash;
+        newUser.save().then((user) => {
+          res.json({
+            user: {
+              firstName: firstName,
+              lastName: lastName,
+              email: email,
+            },
+          });
+        });
+      });
+    });
   });
 });
 
