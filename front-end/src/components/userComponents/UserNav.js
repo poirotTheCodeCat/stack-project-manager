6f1888dc514c5159b7c0377530a8c0c596070872
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-05-13 17:51:30 -0400

    Added User Navigation Component File

---
+++ front-end/src/components/userComponents/UserNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../App.css";
+
+function HomeNav() {
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
+      <div className="container-fluid"></div>
+    </nav>
+  );
+}
+
+export default HomeNav;
